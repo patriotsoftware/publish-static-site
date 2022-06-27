c1c2503ad5119aca684166fdd0986fe81cd74be5
mjarmanps <mjarman@patriotsoftware.com>
2022-06-27 11:10:36 -0400

    adding bucket name and distribution id

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,9 @@
 TESTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Required Variables
-export BUCKET_NAME=""
+export BUCKET_NAME="ps-static-dev"
 export BUCKET_PATH="test/app/path"
-export DISTRIBUTION_ID=""
+export DISTRIBUTION_ID="any-random-id"
 export APP_PATH="$TESTS_DIR/test-app"
 # Optional Variables that can be added to the 
 export ADDITIONAL_BUCKET_ARGS="--dryrun"
